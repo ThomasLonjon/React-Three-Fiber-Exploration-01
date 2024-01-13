9dba12a3c9151f8c6eb1bcea31f169301e53fca9
ThomasLonjon <lonjon.thomas@gmail.com>
2024-01-13 11:24:29 +0100

    added orbit control and a new geometry component

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,42 @@
+import { useRef } from "react";
+import { useThree, useFrame, extend } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import CustomObject from "./CustomObject";
+
 export default function Experience() {
+  extend({ OrbitControls });
+
+  const { camera, gl } = useThree();
+
+  const cubeRef = useRef();
+  const groupRef = useRef();
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta;
+  });
+
   return (
     <>
-      <mesh scale={2} rotation-y={Math.PI * 0.25} position-x={2}>
-        <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" />
-      </mesh>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color="orange" />
-      </mesh>
+      <orbitControls args={[camera, gl.domElement]} />
+
+      <group ref={groupRef}>
+        <mesh ref={cubeRef} scale={2} rotation-y={Math.PI * 0.25} position-x={2}>
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color="orange" />
+        </mesh>
+      </group>
+
       <mesh position-y={-1} scale={10} rotation-x={-Math.PI * 0.5}>
         <planeGeometry />
         <meshBasicMaterial />
       </mesh>
+
+      <CustomObject />
     </>
   );
 }
