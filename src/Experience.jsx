c371483618f0154270052d20efc6c2778bd5bbea
ThomasLonjon <lonjon.thomas@gmail.com>
2024-01-16 15:30:02 +0100

    added text

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Html, OrbitControls, PivotControls } from "@react-three/drei";
+import { Text, Html, OrbitControls, PivotControls } from "@react-three/drei";
 
 export default function Experience()
 {
@@ -9,7 +9,7 @@ export default function Experience()
     const sphereRef = useRef()
 
     // implementing rotation for the cube
-    useFrame((state, delta) => {cubeRef.current.rotation.y += delta})
+    // useFrame((state, delta) => {cubeRef.current.rotation.y += delta})
 
 
 
@@ -47,6 +47,16 @@ export default function Experience()
             <planeGeometry />
             <meshBasicMaterial />
         </mesh>
+
+        <Text 
+            font="./Monofett-Regular.ttf"
+            fontSize={0.5}
+            color="black"
+            position-y={2}
+            textAlign="center"
+        >
+            Studio Carto
+        </Text>
 
     </>
     
