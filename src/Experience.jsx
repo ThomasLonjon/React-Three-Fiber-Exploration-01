845d282269f6e9c64cf173966405374c6eb55703
ThomasLonjon <lonjon.thomas@gmail.com>
2024-01-16 10:47:31 +0100

    changed dependencies

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { TransformControls, OrbitControls } from "@react-three/drei";
+import { OrbitControls, PivotControls } from "@react-three/drei";
 
 export default function Experience()
 {
@@ -16,19 +16,19 @@ export default function Experience()
     return <>
         <OrbitControls makeDefault/>    
 
-        <TransformControls object={cubeRef}/>
-
-            
+           
         <mesh ref={cubeRef} scale={2} rotation-y={Math.PI * 0.25} position-x={2}>
             <boxGeometry />
             <meshBasicMaterial color="mediumpurple" />
         </mesh>
         
-
-        <mesh position-x={-2}>
-            <sphereGeometry />
-            <meshBasicMaterial color="orange" />
-        </mesh>
+        <PivotControls anchor={[0,0,0]} depthTest={false} lineWidth={1} axisColors={ [ '#9381ff', '#ff4d6d', '#7ae582' ] }> 
+            <mesh position-x={-2}>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
+        </PivotControls>
+        
 
 
         <mesh position-y={-1} scale={10} rotation-x={-Math.PI * 0.5}>
